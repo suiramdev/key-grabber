a86c7ba1bb3351f60d6dff7caf377212e69c249f
HamsterOh <mariusnouchet@gmail.com>
2021-04-04 01:15:24 +0200

    Authentify using configured credentials Log on disconnected Code cleanup

--- index.js
+++ index.js
@@ -5,29 +5,33 @@ const SteamUser = require('steam-user');
 const client = new Discord.Client();
 
 const steamUser = new SteamUser();
-steamUser.logOn();
-
-steamUser.on('loggedOn', details => {
-    console.log("Logged onto Steam as " + steamUser.steamID);
+let authentified = false;
+steamUser.logOn({
+    "accountName": config.accountName,
+    "password": config.password
 });
 
-client.on('ready', () => {
-    console.log("Listening to any steam keys...")
+steamUser.on('loggedOn', () => {
+    console.log("Logged onto Steam as " + steamUser.steamID.getSteam3RenderedID());
+    console.log("Listening to keys... ");
+    authentified = true;
+});
+
+steamUser.on('disconnected', () => {
+    console.log("Disconnected of Steam");
+    authentified = false;
 });
 
 client.on('message', message => {
-    if (config.listenServers.includes(message.guild.id)) {
-        const matches = message.content.match(/((\w{5}-){2,}\w{5})/);
-        if (matches) {
-            console.log(`Trying to redeem: ${matches[0]}`);
-            steamUser.redeemKey(matches[0], (err, purchaseResultDetails, packageList) => {
-                if (err) {
-                    console.log("😭 Could not redeem...");
-                } else {
-                    console.log("🎉 Redeemed succesfully !");
-                }
-            });
-        }
+    if (!authentified) return;
+    if (!config.listenServers.includes(message.guild.id)) return;
+
+    const matches = message.content.match(/((\w{5}-){2,}\w{5})/);
+    if (matches) {
+        console.log(`Trying to redeem: ${matches[0]}`);
+        steamUser.redeemKey(matches[0], (err, purchaseResultDetails, packageList) => {
+            console.log(err ? "😭 Could not redeem..." : "🎉 Redeemed succesfully !");
+        });
     }
 });
 
