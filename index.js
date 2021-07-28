41e2b336c9a6c3ee5879ba0e02bf6f70b1d9808b
HamsterOh <mariusnouchet@gmail.com>
2021-07-28 07:08:52 +0200

    Multi accounts compatibility idea folder ignore

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
 const config = require("./config.json");
 const Discord = require('discord.js');
 const SteamUser = require('steam-user');
+const fs = require('fs');
 
 const client = new Discord.Client();
 
-const steamUser = new SteamUser();
 let authentified = false;
-steamUser.logOn({
-    "accountName": config.accountName,
-    "password": config.password
-});
+let nextUser = 0;
 
-steamUser.on('loggedOn', () => {
-    console.log("Logged onto Steam as " + steamUser.steamID.getSteam3RenderedID());
-    console.log("Listening to keys... ");
-    authentified = true;
-});
+function connectToSteam(account, password) {
+    const user = new SteamUser();
 
-steamUser.on('disconnected', () => {
-    console.log("Disconnected of Steam");
-    authentified = false;
-});
+    user.logOn({
+        "accountName": account,
+        "password": password
+    });
 
+    user.on('loggedOn', () => {
+        log("Logged onto Steam as " + user.steamID.getSteam3RenderedID());
+        log("Listening to keys... ");
+        authentified = true;
+    });
+
+    user.on('disconnected', () => {
+        log("Disconnected of Steam");
+        authentified = false;
+    });
+
+    return user;
+}
+
+function nextUser() {
+    const account = config.accounts[nextUser];
+    nextUser++;
+
+    return connectToSteam(account.username, account.password);
+}
+
+function log(text) {
+    console.log(text);
+    fs.appendFile('log.txt', text + "\n", (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+let steamUser = nextUser(accounts);
 client.on('message', message => {
-    if (!authentified) return;
-    if (!config.listenServers.includes(message.guild.id)) return;
+    if (!authentified)
+        steamUser = nextUser(accounts);
+    if (!config.listenServers.includes(message.guild.id))
+        return;
 
-    const matches = message.content.match(/((\w{5}-){2,}\w{5})/);
+    const date = new Date();
+    log("<" + date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ">" + steamUser.accountInfo.name + " ----> " + message.guild.name + " - Channel : " + message.channel.name + ", By : " + message.author.username + " -> " + message.content);
+
+    const matches = message.content.match(/((\w{5}-){2,5}\w{5})/);
     if (matches) {
-        console.log(`Trying to redeem: ${matches[0]}`);
+        log(`Trying to redeem: ${matches[0]}`);
         steamUser.redeemKey(matches[0], (err, purchaseResultDetails, packageList) => {
-            console.log(err ? "😭 Could not redeem..." : "🎉 Redeemed succesfully !");
+            if (!err) {
+                log("🎉 Redeemed succesfully ! : " + matches[0]);
+                steamUser.logOff();
+                steamUser = nextUser(accounts);
+            } else {
+                log("😭 Could not redeem...");
+            }
         });
     }
 });
-
 client.login(config.token);
